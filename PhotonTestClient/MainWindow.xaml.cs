cd91ef3d93adbaaf1456d5722d81f35b4a14d53d
Sebastian Kenter <info@nethersoft.de>
2016-01-08 21:54:38 +0100

    First Test Client Version

---
+++ PhotonTestClient/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using ExitGames.Client.Photon;
+
+namespace PhotonTestClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PhotonPeer Peer { get; set; }
+        public bool PeerConnecting { get; set; }
+        public bool PeerConnected { get; set; }
+        public DispatcherTimer DispatcherTimer { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PeerConnecting = false;
+            PeerConnected = false;
+            PhotonListener listener = new PhotonListener {Main = this};
+            Peer = new PhotonPeer(listener, ConnectionProtocol.Udp);
+
+            DisconnectButton.IsEnabled = false;
+            DispatcherTimer = new DispatcherTimer();
+            DispatcherTimer.Tick += dispatcherTimer_tick;
+            DispatcherTimer.Interval = new TimeSpan(0,0,0,1);
+            StatusIndicator.Fill = new SolidColorBrush(Colors.Red);
+            StatusTextBox.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+
+        private void ConnectButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            StatusTextBox.AppendText("Trying to connect to " + IPAddress.Text + " on default port 5055...\n");
+            ConnectButton.IsEnabled = false;
+            if (Peer.Connect(IPAddress.Text + ":5055", "MasterServer"))
+            {
+                PeerConnecting = true;
+                StatusTextBox.AppendText("Connection initialization in progress....\n");
+                DispatcherTimer.Start();
+            }
+            else
+            {
+                StatusTextBox.AppendText("Connection initialization failed...\n");
+            }
+        }
+
+        private void DisconnectButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            StatusTextBox.AppendText("Trying to disconnect from the server at " + IPAddress.Text + "...\n");
+            Peer.Disconnect();
+        }
+
+        private void dispatcherTimer_tick(object sender, EventArgs e)
+        {
+            Service();
+            if (PeerConnected)
+            {
+                StatusIndicator.Fill = new SolidColorBrush(Colors.Green);
+            }
+            else if (PeerConnecting && !PeerConnected)
+            {
+                StatusIndicator.Fill = new SolidColorBrush(Colors.Orange);
+            }
+            else
+            {
+                StatusIndicator.Fill = new SolidColorBrush(Colors.Red);
+            }
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private void Service()
+        {
+            Peer.Service();
+        }
+
+    }
+}
---
+++ PhotonTestClient/PhotonListener.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using ExitGames.Client.Photon;
+
+namespace PhotonTestClient
+{
+    public class PhotonListener : IPhotonPeerListener
+    {
+        public MainWindow Main;
+
+        public void DebugReturn(DebugLevel level, string message)
+        {
+            Main.Dispatcher.Invoke(
+                () => { Main.StatusTextBox.AppendText("DebugReturn called with: " + level + " - " + message + "\n"); });
+        }
+
+        public void OnOperationResponse(OperationResponse operationResponse)
+        {
+            Main.Dispatcher.Invoke(() =>
+            {
+                Main.StatusTextBox.AppendText("OnOperationResponse called with: " + operationResponse.OperationCode +
+                                              " - " + operationResponse.ReturnCode +
+                " - " + operationResponse.DebugMessage + "\n")
+                ;
+            });
+        }
+
+        public void OnStatusChanged(StatusCode statusCode)
+        {
+            Main.Dispatcher.Invoke(
+                () => Main.StatusTextBox.AppendText("OnStatusChanged called with " + statusCode + "\n"));
+
+            if (statusCode == StatusCode.Connect)
+            {
+                Main.DisconnectButton.IsEnabled = true;
+                Main.PeerConnected = true;
+            }
+            else
+            {
+                Main.DisconnectButton.IsEnabled = false;
+                Main.ConnectButton.IsEnabled = true;
+                Main.StatusIndicator.Fill = new SolidColorBrush(Colors.Red);
+                Main.PeerConnecting = false;
+                Main.PeerConnecting = false;
+                Main.DispatcherTimer.Stop();
+            }
+        }
+
+        public void OnEvent(EventData eventData)
+        {
+            Main.Dispatcher.Invoke(() => Main.StatusTextBox.AppendText("OnEvent called with " + eventData.Code));
+        }
+    }
+}
